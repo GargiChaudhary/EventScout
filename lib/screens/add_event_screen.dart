c29dc8f95cb8265293a362b7f3a4f5310c71057a
Gargi Chaudhary <gargichaudhary06@gmail.com>
2023-10-23 21:09:44 +0530

    commit

--- lib/screens/add_event_screen.dart
+++ lib/screens/add_event_screen.dart
@@ -1,10 +1,59 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 
-class AddEventScreen extends StatelessWidget {
+import 'package:events/model/our_user.dart';
+import 'package:events/providers/user_provider.dart';
+import 'package:events/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+
+class AddEventScreen extends StatefulWidget {
   const AddEventScreen({super.key});
 
   @override
+  State<AddEventScreen> createState() => _AddEventScreenState();
+}
+
+class _AddEventScreenState extends State<AddEventScreen> {
+  Uint8List? _file;
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Select Event Picture:'),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose from gallery'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // final OurUser ourUser = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 70,
@@ -48,10 +97,61 @@ class AddEventScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [],
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                        'https://www.euroschoolindia.com/blogs/wp-content/webp-express/webp-images/uploads/2023/08/cartoons-for-kids.jpg.webp'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Stack(children: [
+                    Visibility(
+                      visible: _file != null,
+                      child: Container(
+                        height: MediaQuery.of(context).size.width * 0.75,
+                        width: MediaQuery.of(context).size.width * 0.75,
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage(
+                                    'https://www.euroschoolindia.com/blogs/wp-content/webp-express/webp-images/uploads/2023/08/cartoons-for-kids.jpg.webp'))),
+                      ),
+                    ),
+                    Visibility(
+                      visible: _file == null,
+                      child: GestureDetector(
+                        onTap: () => _selectImage(context),
+                        child: Container(
+                          height: MediaQuery.of(context).size.width * 0.75,
+                          width: MediaQuery.of(context).size.width * 0.75,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                              image: const DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image:
+                                      AssetImage('assets/images/select.png'))),
+                        ),
+                      ),
+                    )
+                  ])
+                ],
+              ),
+            ],
           ),
         ),
       ),
