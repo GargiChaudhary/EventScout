f1e776c5585f3cc79a95be246e1daf590e90b9d5
Gargi Chaudhary <gargichaudhary06@gmail.com>
2023-11-24 19:55:15 +0530

    added search events by distance functionality

--- lib/screens/search_by_distance.dart
+++ lib/screens/search_by_distance.dart
@@ -82,7 +82,7 @@ class _SearchByDistanceState extends State<SearchByDistance> {
     });
   }
 
-  double maxDistance = 0;
+  double maxDistance = double.infinity;
 
   Future<List<Map<String, dynamic>>> _getFilteredEvents() async {
     final eventsCollection = FirebaseFirestore.instance.collection('events');
@@ -95,7 +95,7 @@ class _SearchByDistanceState extends State<SearchByDistance> {
     List<Map<String, dynamic>> filteredEvents = [];
     querySnapshot.docs.forEach((eventDoc) {
       // Access event data as a map
-      Map<String, dynamic> eventData = eventDoc.data() ?? {};
+      Map<String, dynamic> eventData = eventDoc.data();
 
       double eventLatitude = eventData['latitude'] ?? 0.0;
       double eventLongitude = eventData['longitude'] ?? 0.0;
@@ -116,10 +116,6 @@ class _SearchByDistanceState extends State<SearchByDistance> {
 
     return filteredEvents;
   }
-
-  // print("Event lat is: $eventLatitude");
-  // print("Event long is: $eventLongitude");
-  // print("The distance between is: $distance");
 
   @override
   Widget build(BuildContext context) {
@@ -148,7 +144,7 @@ class _SearchByDistanceState extends State<SearchByDistance> {
                   InkWell(
                       onTap: () {
                         setState(() {
-                          maxDistance = 0;
+                          maxDistance = double.infinity;
                         });
                         // print("max dis is: $maxDistance");
                       },
@@ -169,25 +165,6 @@ class _SearchByDistanceState extends State<SearchByDistance> {
                         // print("max dis is: $maxDistance");
                       },
                       child: const MyButton(text: "50 km")),
-
-                  // ElevatedButton(
-                  //   style: ButtonStyle(
-                  //       backgroundColor: MaterialStatePropertyAll(
-                  //           Theme.of(context).focusColor)),
-                  //   child: Text(
-                  //     "70 km",
-                  //     style: TextStyle(
-                  //         fontFamily: 'Montserrat',
-                  //         fontSize: 13,
-                  //         fontWeight: FontWeight.w500,
-                  //         color: Theme.of(context).hintColor),
-                  //   ),
-                  //   onPressed: () {
-                  //     setState(() {
-                  //       maxDistance = 70;
-                  //     });
-                  //   },
-                  // ),
                 ],
               ),
               const SizedBox(
@@ -255,167 +232,158 @@ class _SearchByDistanceState extends State<SearchByDistance> {
                 thickness: 0.2,
               ),
 
-              SingleChildScrollView(
-                child: FutureBuilder(
-                  future: _getFilteredEvents(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                      return Center(
-                          child: Text('No events within $maxDistance km'));
-                    } else {
-                      return ListView.builder(
-                        itemCount: (snapshot.data! as dynamic).docs.length,
-                        itemBuilder: (context, index) {
-                          // DocumentSnapshot eventDoc =
-                          //     (snapshot.data! as dynamic).docs[index];
-                          // DocumentSnapshot<Object?> eventDoc =
-                          //     snapshot.data![index];
-
-                          DateTime eventDate = DateFormat('yyyy-MM-dd').parse(
-                              (snapshot.data! as dynamic).docs[index]
-                                  ['duration']);
-                          return InkWell(
-                            onTap: () => Navigator.of(context).push(
-                                MaterialPageRoute(
-                                    builder: (context) => EventDetailsPage(
-                                        event: Event.fromSnap(
-                                            (snapshot.data! as dynamic)
-                                                .docs[index][index])))),
-                            child: Container(
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              decoration: BoxDecoration(
-                                // color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.9),
-                                // boxShadow: [
-                                //   BoxShadow(
-                                //     color: Theme.of(context).shadowColor.withOpacity(0.3),
-                                //     spreadRadius: 0.2,
-                                //     blurRadius: 3,
-                                //     offset: const Offset(0, 3),
-                                //   )
-                                // ],
-                                // border: Border.all(
-                                //   color: Theme.of(context).shadowColor.withOpacity(0.5),
-                                //   width: 0.6,
-                                // ),
-                                // borderRadius: BorderRadius.circular(8)
-                                border: Border.all(
-                                    width: 0.5,
-                                    color: Theme.of(context)
-                                        .hintColor
-                                        .withAlpha(80)),
-                                borderRadius: BorderRadius.circular(15),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Theme.of(context)
-                                        .focusColor
-                                        .withAlpha(100),
-                                    blurRadius: 10.0,
-                                    spreadRadius: 0.0,
-                                  ),
-                                ],
-                                color: Theme.of(context)
-                                    .primaryColor
-                                    .withOpacity(0.2),
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.4,
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          capitalizeAllWord(
-                                              (snapshot.data! as dynamic)
-                                                  .docs[index]['title']),
-                                          style: TextStyle(
-                                              fontFamily: 'Montserrat',
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.w600,
-                                              color:
-                                                  Theme.of(context).hintColor),
-                                          softWrap: true,
-                                        ),
-                                        Row(
-                                          children: [
-                                            const Icon(
-                                              Icons.location_on,
-                                              size: 15,
-                                              color: Colors.red,
-                                            ),
-                                            const SizedBox(
-                                              width: 5,
-                                            ),
-                                            Flexible(
-                                              child: Text(
-                                                capitalizeAllWord(
-                                                    (snapshot.data! as dynamic)
-                                                            .docs[index]
-                                                        ['location']),
-                                                style: TextStyle(
-                                                    fontWeight: FontWeight.w500,
-                                                    fontFamily: 'Montserrat',
-                                                    fontSize: 12,
-                                                    color: Theme.of(context)
-                                                        .hintColor),
-                                                softWrap: true,
-                                              ),
-                                            )
-                                          ],
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                  ElevatedButton(
-                                    style: ButtonStyle(
-                                      backgroundColor:
-                                          MaterialStateProperty.all<Color>(
-                                              Theme.of(context).primaryColor),
-                                    ),
-                                    onPressed: () {},
-                                    child: FittedBox(
-                                      child: Row(
+              FutureBuilder(
+                future: _getFilteredEvents(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(
+                        child: Text('No events within $maxDistance km'));
+                  } else {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> eventData = snapshot.data![index];
+                        Event event = Event(
+                          title: eventData['title'],
+                          uid: eventData['uid'],
+                          username: eventData['username'],
+                          eventId: eventData['eventId'],
+                          datePublished: eventData['datePublished'],
+                          profImage: eventData['profImage'],
+                          bio: eventData['bio'],
+                          description: eventData['description'],
+                          eventUrl: eventData['eventUrl'],
+                          location: eventData['location'],
+                          duration: eventData['duration'],
+                          punchLine: eventData['punchLine'],
+                          latitude: eventData['latitude'],
+                          longitude: eventData['longitude'],
+                          categoryIds: eventData['categoryIds'].cast<int>(),
+                          galleryImages:
+                              eventData['galleryImages'].cast<String>(),
+                        );
+                        DateTime eventDate = DateFormat('yyyy-MM-dd')
+                            .parse(eventData['duration']);
+                        return InkWell(
+                          onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      EventDetailsPage(event: event))),
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 0.5,
+                                  color: Theme.of(context)
+                                      .hintColor
+                                      .withAlpha(80)),
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Theme.of(context)
+                                      .focusColor
+                                      .withAlpha(100),
+                                  blurRadius: 10.0,
+                                  spreadRadius: 0.0,
+                                ),
+                              ],
+                              color: Theme.of(context)
+                                  .primaryColor
+                                  .withOpacity(0.2),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        capitalizeAllWord(eventData['title']),
+                                        style: TextStyle(
+                                            fontFamily: 'Montserrat',
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600,
+                                            color: Theme.of(context).hintColor),
+                                        softWrap: true,
+                                      ),
+                                      Row(
                                         children: [
-                                          Icon(
-                                            Icons.access_time,
-                                            size: 14,
-                                            color: Theme.of(context).hintColor,
+                                          const Icon(
+                                            Icons.location_on,
+                                            size: 15,
+                                            color: Colors.red,
                                           ),
                                           const SizedBox(
                                             width: 5,
                                           ),
-                                          Text(
-                                            calculateTimeLeft(eventDate),
-                                            style: TextStyle(
-                                                fontFamily: 'Montserrat',
-                                                fontSize: 12,
-                                                color: Theme.of(context)
-                                                    .hintColor),
+                                          Flexible(
+                                            child: Text(
+                                              capitalizeAllWord(
+                                                  eventData['location']),
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w500,
+                                                  fontFamily: 'Montserrat',
+                                                  fontSize: 12,
+                                                  color: Theme.of(context)
+                                                      .hintColor),
+                                              softWrap: true,
+                                            ),
                                           )
                                         ],
-                                      ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Theme.of(context).primaryColor),
+                                  ),
+                                  onPressed: () {},
+                                  child: FittedBox(
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.access_time,
+                                          size: 14,
+                                          color: Theme.of(context).hintColor,
+                                        ),
+                                        const SizedBox(
+                                          width: 5,
+                                        ),
+                                        Text(
+                                          calculateTimeLeft(eventDate),
+                                          style: TextStyle(
+                                              fontFamily: 'Montserrat',
+                                              fontSize: 12,
+                                              color:
+                                                  Theme.of(context).hintColor),
+                                        )
+                                      ],
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          );
-                        },
-                      );
-                    }
-                  },
-                ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
               ),
             ],
           ),
